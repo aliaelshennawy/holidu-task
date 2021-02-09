68e15a90d8e32497c1b5852f59cd97c033fe1dad
[alia.elshennawy1] <alia.elshennawy@thndr.app>
2021-02-09 22:49:10 +0200

    chore: add axios

--- holidu-task/src/sagas/index.js
+++ holidu-task/src/sagas/index.js
@@ -1,15 +1,19 @@
-import { put, takeLatest, all } from 'redux-saga/effects';
+import axios from 'axios'
+import { call, put, all, takeLatest } from 'redux-saga/effects'
+
 function* fetchOffers() {
-  const json = yield fetch('https://api.holidu.com/old/rest/v6/search/offers?searchId=46fdd790-e0a7-4b96-98b5-6be594a8e8b1&searchTerm=Mallorca%2C+Spanien&adults=2&domainId=2&locale=de-DE&currency=EUR')
-        .then(response => response.json(), );    
-        //check offers to replace value below
-  yield put({ type: "OFFERS_RECEIVED", json: json.offers, });
+  try {
+    const { data } = yield call(axios.get, 'https://api.holidu.com/old/rest/v6/search/offers?searchId=46fdd790-e0a7-4b96-98b5-6be594a8e8b1&searchTerm=Mallorca%2C+Spanien&adults=2&domainId=2&locale=de-DE&currency=EUR')
+    yield put({ type: "OFFERS_RECEIVED", data})
+  } catch (e) {
+    console.log(e.message)
+  }
 }
 function* actionWatcher() {
-     yield takeLatest('GET_OFFERS', fetchOffers)
+  yield takeLatest('GET_OFFERS', fetchOffers)
 }
 export default function* rootSaga() {
-   yield all([
-   actionWatcher(),
-   ]);
+yield all([
+actionWatcher(),
+]);
 }
