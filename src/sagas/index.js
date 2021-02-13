a53a442bbe4676560be5f2dc67ef81c33cff5236
[alia.elshennawy1] <alia.elshennawy@thndr.app>
2021-02-13 22:28:39 +0200

    fix: cancel request if another happens

--- src/sagas/index.js
+++ src/sagas/index.js
@@ -1,31 +1,41 @@
-import axios from 'axios'
-import { call, put, all, takeLatest, select } from 'redux-saga/effects'
-import { getQuery } from '../actions'
+import axios from "axios";
+import { call, put, all, takeLatest, select } from "redux-saga/effects";
+import { getQuery } from "../actions";
 
-
+const CancelToken = axios.CancelToken;
+let source = CancelToken.source();
 
 function* fetchOffers() {
-  let query = yield select(getQuery)
+  let query = yield select(getQuery);
+  source && source.cancel("Operation canceled due to new request.");
+  source = axios.CancelToken.source();
   try {
-    const { data } = yield call(axios.get, 'https://api.holidu.com/old/rest/v6/search/offers', {
-      params: query
-  })
-    if(data.offers.length) {
-      yield put({ type: "OFFERS_RECEIVED", data})
+    const { data } = yield call(
+      axios.get,
+      "https://api.holidu.com/old/rest/v6/search/offers",
+      {
+        params: query,
+      },
+      {
+        cancelToken: source.token,
+      }
+    );
+    if (data.offers.length) {
+      yield put({ type: "OFFERS_RECEIVED", data });
+    } else {
+      yield put({ type: "NO_OFFERS_RECIEVED" });
     }
-    else {
-      yield put({ type: "NO_OFFERS_RECIEVED"})
-    }
-    return Promise.resolve('Offers Retrived Successfully');
+    return Promise.resolve("Offers Retrived Successfully");
   } catch (e) {
+    if (axios.isCancel(e)) {
+      console.log('Request canceled');
+    }
     return Promise.reject(e);
   }
 }
 function* actionWatcher() {
-  yield takeLatest('GET_OFFERS', fetchOffers)
+  yield takeLatest("GET_OFFERS", fetchOffers);
 }
 export default function* rootSaga() {
-yield all([
-actionWatcher(),
-]);
+  yield all([actionWatcher()]);
 }
